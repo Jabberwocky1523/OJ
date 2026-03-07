80a65d14c7686b731f7b76dbacca36c60117a793
Jabberwocky <wangy5236@gmail.com>
2026-03-07 20:12:39 +0800

    new file:   .gitignore 	modified:   exam/2020_3.cpp

--- exam/2020_3.cpp
+++ exam/2020_3.cpp
@@ -2,13 +2,12 @@
 using namespace std;
 struct cylinder
 {
-    long long R, H;   
+    long long R, H;
 };
 int main()
 {
     ios::sync_with_stdio(false);
     int n, k;
     cin >> n >> k;
-
     return 0;
 }
