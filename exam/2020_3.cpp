a0b5d207c3a305d924fd2605eea13f65f9b36454
Jabberwocky <wangy5236@gmail.com>
2026-03-08 12:30:59 +0800

    modified:   .vscode/c_cpp_properties.json 	modified:   .vscode/settings.json 	modified:   exam/2020_3.cpp 	new file:   exam/2020_4.cpp 	new file:   exam/2020_5.cpp

--- exam/2020_3.cpp
+++ exam/2020_3.cpp
@@ -3,11 +3,43 @@ using namespace std;
 struct cylinder
 {
     long long R, H;
+    cylinder(long long _R, long long _H)
+    {
+        R = _R;
+        H = _H;
+    }
+    long long vo()
+    {
+        return H * pow(R, 2);
+    }
 };
+void bfs(vector<cylinder> v, int &sum, int ii, int &max, int depth, int k)
+{
+    if (depth == k)
+    {
+        return;
+    }
+    for (int i = ii; i < v.size(); i++)
+    {
+        
+    }
+}
+bool cmp(cylinder c1, cylinder c2)
+{
+    return c1.R < c2.R;
+}
 int main()
 {
     ios::sync_with_stdio(false);
     int n, k;
     cin >> n >> k;
+    vector<cylinder> v;
+    for (int i = 0; i < n; i++)
+    {
+        long long R, H;
+        cin >> R >> H;
+        v.push_back(cylinder(R, H));
+    }
+    sort(v.begin(), v.end(), cmp);
     return 0;
 }
---
+++ exam/2020_5.cpp
@@ -0,0 +1,56 @@
+#include <bits/stdc++.h>
+using namespace std;
+string add(string s1, string s2)
+{
+    if (s1.length() > s2.length())
+    {
+        swap(s1, s2);
+    }
+    reverse(s1.begin(), s1.end());
+    reverse(s2.begin(), s2.end());
+    long long c = 0;
+    string ans;
+    for (int i = 0; i < s1.length(); i++)
+    {
+        int temp = s1[i] - '0' + s2[i] - '0' + c;
+        ans.push_back(temp % 10 + '0');
+        c = temp / 10;
+    }
+    for (int i = s1.length(); i < s2.length(); i++)
+    {
+        int temp = s2[i] - '0' + c;
+        ans.push_back(temp % 10 + '0');
+        c = temp / 10;
+    }
+    if (c != 0)
+        ans.push_back(c + '0');
+    reverse(ans.begin(), ans.end());
+    return ans;
+};
+bool a(string s)
+{
+    string temp = s;
+    reverse(temp.begin(), temp.end());
+    return s == temp;
+}
+int main()
+{
+    ios::sync_with_stdio(false);
+    long long n;
+    cin >> n;
+    string s1 = to_string(n), s2 = to_string(n);
+    reverse(s2.begin(), s2.end());
+    int sum = 0;
+    string ans = add(s1, s2);
+    while (!a(ans))
+    {
+        /* code */
+        cout << ans << endl;
+        string temp = ans;
+        reverse(temp.begin(), temp.end());
+        ans = add(ans, temp);
+        sum++;
+    }
+    cout << sum << " " << ans;
+    return 0;
+}
