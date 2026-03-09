6a99845ccbc54d86894366df4c8168c49c2a0e9b
Jabberwocky1523 <439887070@qq.com>
2026-03-09 19:18:41 +0800

    modified:   exam/2021_4.cpp

--- exam/2021_4.cpp
+++ exam/2021_4.cpp
@@ -3,6 +3,13 @@ using namespace std;
 int main()
 {
     ios::sync_with_stdio(false);
-    
+    int n, s;
+    cin >> n >> s;
+    vector<int> v(n);
+    for (int i = 0; i < n; i++)
+    {
+        cin >> v[i];
+    }
+    v.push_back(s);z
     return 0;
 }
